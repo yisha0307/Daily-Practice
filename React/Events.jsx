a4e87772f5306b3999544913381d1c9d00f9c759
Yisha Chen <chenchen9037@126.com>
2016-12-08 18:07:50 +0800

    Create Events.jsx

---
+++ React/Events.jsx
@@ -0,0 +1,16 @@
+//example 1
+function ActionLink(){
+  function handleClick(e){
+    e.preventDefault();
+    console.log('The link was clicked.');
+  }
+  return (
+    <a href ="#" onClick = {handleClick}>
+      Click me
+      </a>
+  );
+}
+
+ReactDOM.render(
+<ActionLink/>,
+document.body);
