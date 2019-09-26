01c6b92a5341d02e691f9052bd023f97d6809ece
chenyisha <chenchen9037@126.com>
2019-09-26 10:46:59 +0800

    finish 8&&9

--- JS-pratice/20190926.js
+++ JS-pratice/20190926.js
@@ -12,3 +12,8 @@ function strEncode(str) {
         return String.fromCharCode(s.charCodeAt() + 1)
     }).join("")
 }
+
+// day9 写一个判断数据类型的方法
+function whichType(obj) {
+    return Object.prototype.toString.call(obj).replace(/\[object\s|\]/g, '')
+}
