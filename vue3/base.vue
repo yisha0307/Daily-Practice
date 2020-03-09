b5532bbb18de98694c916cfc4aee2aeb577f7989
chenyisha <chenchen9037@126.com>
2020-03-09 16:43:21 +0800

    add'

---
+++ vue3/base.vue
@@ -0,0 +1,24 @@
+<template>
+    <button @click='increment'>
+        count is: {{state.count}}, double is: {{state.double}}
+    </button>
+</template>
+
+<script>
+    import {reactive, computed} from 'vue'
+    export default {
+        setup() {
+            const state = reactive({
+                count: 0,
+                double: computed(() => state.count*2)
+            })
+            function increment() {
+                state.count++
+            }
+            return {
+                state,
+                increment
+            }
+        }
+    }
+</script>
